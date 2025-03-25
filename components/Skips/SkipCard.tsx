f2805f66a1e53c9f746979fa543437a685dfe8de
ahmadshaker1 <74874913+ahmadshaker1@users.noreply.github.com>
2025-03-25 14:30:50 +0200

    Fixed an issue with type safety and added env variables

--- components/Skips/SkipCard.tsx
+++ components/Skips/SkipCard.tsx
@@ -29,7 +29,7 @@ const SkipCard: React.FC<SkipCardProps> = ({
   const totalPrice =
     typeof price_before_vat === "number" && typeof vat === "number"
       ? price_before_vat + (price_before_vat * vat) / 100
-      : 0;
+      : null;
 
   return (
     <div className="rounded-lg p-4">
@@ -124,7 +124,7 @@ const SkipCard: React.FC<SkipCardProps> = ({
       </div>
       <div className="flex flex-row items-center justify-between w-full mt-4">
         <p className="text-xl font-semibold">
-          £{totalPrice?.toFixed?.(2) ?? "0.0"}
+          £{typeof totalPrice === "number" ? totalPrice.toFixed(2) : "N/A"}
         </p>
 
         <button className="cursor-pointer duration-300 hover:brightness-90 group w-18 text-xs font-semibold bg-[#22c55e] text-white pl-3 py-1 rounded-full flex flex-row items-center justify-start">
